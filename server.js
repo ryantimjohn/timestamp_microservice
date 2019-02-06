246dee885201598501fd2047e72f47f15840dff0
Glitch (ring-quail) <none>
2019-02-06 20:42:15 +0000

    🏦👅 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/timestamp/:date_string?", (req, res)=>{
+  var date = new Date();
+  if (req.params.date_string) {date = new Date(req.params.date_string)};
+  if (date.toString() == "Invalid Date"){
+    date = new Date(parseInt(req.params.date_string))
+    if (date.toString() == "Invalid Date"){res.json({error: "Invalid Date"})}
+    else {res.json({unix: date.getTime(), utc: date.toUTCString()})}
+  }
+  else {res.json({unix: date.getTime(), utc: date.toUTCString()})};
+})
